6e88c180c14d6133300d2752589f6ad15724121f
wolfGhost23 <crvargasm@unal.edu.co>
2022-05-26 01:42:02 -0500

    Correo Funcional

--- email.php
+++ email.php
@@ -1,14 +1,20 @@
 <?php
 // El mensaje
-$paraCorreo = "kmilito2002@gmail.com";
+?>
+<form action="email.php" method="post">
+    <input type="text" name="correo" placeholder="Correo"></input>
+    <button type="submit" name="send" >Enviar</button>
+</form>
+<?php
+$paraCorreo = $_POST['correo'] ?? '';
 $titulo = "Recuperación de Contraseña";
 $mensaje = "Hola :D";
 $tuCorreo = "From: noresponderEliot@gmail.com";
 
 if (mail($paraCorreo, $titulo, $mensaje, $tuCorreo)) {
     echo "Correo Envíado";
-} else {
-    echo "eroor";
+} else if (isset($_POST['send'])){
+    echo "Error :P";
 }
 
 ?>
